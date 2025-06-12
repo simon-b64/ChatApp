58bd37f3843853723287428fa7b579fe641f2db3
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-06-12 11:24:22 +0200

    chore: Adapted indent_size

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+    ],
 };
