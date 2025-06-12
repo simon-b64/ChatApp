4fb2e2ff3a9969fbd39c32c35c8b667c93aa4986
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-06-12 11:50:59 +0200

    feat: Added not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotFoundPage } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'not-found',
+        component: NotFoundPage,
+    },
+    {
+        path: '**',
+        redirectTo: '/not-found',
+    },
+];
