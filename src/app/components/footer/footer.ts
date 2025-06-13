4253778e3b11e5d13cba8399f69e4d3a4b261f56
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-06-13 11:16:14 +0200

    refactor: Refactored layout for custom footer and better standards

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,45 +1,10 @@
-import {Component, inject} from '@angular/core';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import { faSmile } from '@fortawesome/free-regular-svg-icons';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MessageStore} from '../../stores/message.store';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
-    imports: [
-        FontAwesomeModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
+    imports: [],
     templateUrl: './footer.html',
     styleUrl: './footer.css'
 })
 export class Footer {
-    private readonly formBuilder = inject(FormBuilder);
-    private readonly messageStore = inject(MessageStore);
-
-    messageForm: FormGroup;
-
-    constructor() {
-        this.messageForm = this.formBuilder.group({
-            message: ['', [Validators.required, Validators.maxLength(5120)]],
-        });
-    }
-
-    sendMessage() {
-        if (this.messageForm.invalid) {
-            return;
-        }
-
-        this.messageStore.appendMessage({
-            content: this.messageForm.value.message.trim(),
-            author: 'User',
-            timestamp: new Date()
-        });
-        this.messageForm.reset();
-    }
-
-    protected faPaperPlane = faPaperPlane;
-    protected faSmile = faSmile;
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,18 +1,50 @@
 import {AfterViewChecked, Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {MessageStore} from '../../stores/message.store';
 import {DatePipe} from '@angular/common';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faSmile } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-home',
     imports: [
-        DatePipe
+        DatePipe,
+        FaIconComponent,
+        ReactiveFormsModule
     ],
     templateUrl: './home.html',
     styleUrl: './home.css'
 })
 export class HomePage implements AfterViewChecked {
     protected readonly messageStore = inject(MessageStore);
+    private readonly formBuilder = inject(FormBuilder);
+
     @ViewChild('messagesContainer') private readonly messagesContainer!: ElementRef;
+
+    messageForm: FormGroup;
+
+    constructor() {
+        this.messageForm = this.formBuilder.group({
+            message: ['', [Validators.required, Validators.maxLength(5120)]],
+        });
+    }
+
+    sendMessage() {
+        if (this.messageForm.invalid) {
+            return;
+        }
+
+        this.messageStore.appendMessage({
+            content: this.messageForm.value.message.trim(),
+            author: 'User',
+            timestamp: new Date()
+        });
+        this.messageForm.reset();
+    }
+
+    protected faPaperPlane = faPaperPlane;
+    protected faSmile = faSmile;
 
     ngAfterViewChecked() {
         this.scrollToBottom();
